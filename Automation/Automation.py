af899d9944d377ce2f13d953cbdf8544f034b936
Bradley <bradleytanwork2006@gmail.com>
2025-01-26 17:48:24 +0800

    Boiler plate code for selenium

--- Automation/Automation.py
+++ Automation/Automation.py
@@ -1,5 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service #there is a error, but I don't know why it cannot seem to import
+from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait #This is so that code won't trigger in case page doesn't load (internet connection lost or CAPTCHA)
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+'''
+If you encounter an 'unable to import' issue, just ignore it and run the code
+'''
+
+service = Service('../drivers/chromedriver.exe')
+drivers = webdriver.Chrome(service=service)
+
+drivers.get('https://www.google.com')
+
