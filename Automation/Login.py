09871c778428489850780dba87e28377ba9441c3
Bradley <bradleytanwork2006@gmail.com>
2025-02-02 16:44:14 +0800

    Added headless mode to Testing branch, so that browser window wont open each time I run the code

--- Automation/Login.py
+++ Automation/Login.py
@@ -1,6 +1,7 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,11 +17,16 @@ from dotenv import find_dotenv, load_dotenv
 #Get the absolute path to the chromedriver.exe, because just defining the path to get it to work in a remote repo doesn't work cause we don't know which file will run [future proofing]
 script_dir = os.path.dirname(os.path.abspath(__file__))
 driver_path = os.path.join(script_dir, '../drivers/chromedriver.exe')
+
+# Set Chrome options to enable headless mode, basically running the script without opening a browser window
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")  # Optional: Disable GPU acceleration
+chrome_options.add_argument("--window-size=1920,1080")  # Optional: Set window size
+
 service = Service(driver_path)
-driver = webdriver.Chrome(service=service)
-
+driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get('https://www.nlb.gov.sg/seatbooking/') #Code starts from this landing page so integrating it into code will be easier
-
 
 #First, before we log in to the correct account, we need to get to the login page, and logoout if it is logged in
 try:
@@ -31,7 +37,7 @@ except NoSuchElementException: #Means the current seat booking page is logged in
     Account_Button = driver.find_elements(By.XPATH, "//*[contains(text(), 'Account')]")
     Account_Button[0].click() #Goes to the account page, the only way to log out
     Logout_Button = driver.find_element(By.CLASS_NAME, "v-icon.notranslate.mdi.mdi-logout-variant.theme--dark")
-    Logout_Button.click() 
+    driver.execute_script("arguments[0].click();", Logout_Button)
 
 #Loading in envrioment variables
 dotenv_path = find_dotenv()
